93855c4faffe5598eaff945e78fcf277eb7c0d62
sachinthanimesh370 <sachinthanimesh370@gmail.com>
2024-09-09 22:28:42 +0530

    Install basic libraries

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+function App(): React.JSX.Element {
+  
+  return (
+    <View>
+      <Text>Attendance</Text>
+    </View>
+    
+  );
+}
+
+
+
+
+export default App;
