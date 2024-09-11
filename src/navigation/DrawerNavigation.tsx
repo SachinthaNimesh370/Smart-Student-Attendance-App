113135622a4b32aef27c7a9dcf72281748af10c0
sachinthanimesh370 <sachinthanimesh370@gmail.com>
2024-09-11 18:52:19 +0530

    Create stack navigator and connect with drawer and stack navigator

--- src/navigation/DrawerNavigation.tsx
+++ src/navigation/DrawerNavigation.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { Button, View } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import Location from '../screens/Location';
@@ -11,12 +10,12 @@ const Drawer = createDrawerNavigator();
 
 const Drawer_Navigation = () => {
   return (
-    <NavigationContainer>
+    
       <Drawer.Navigator initialRouteName="Home">
         <Drawer.Screen name="Home" component={Home} />
         <Drawer.Screen name="Location" component={Location} />
       </Drawer.Navigator>
-    </NavigationContainer>
+    
   )
 }
 
---
+++ src/navigation/StackNavigation.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { CardStyleInterpolators, createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import Login from '../screens/Login';
+import Drawer_Navigation from './DrawerNavigation';
+
+
+const StackNavigation = () => {
+    const Stack = createStackNavigator();
+    return (
+      <NavigationContainer >
+          <Stack.Navigator screenOptions={{headerShown:false}}>
+              <Stack.Screen name="Login" component={Login} options={
+                  {cardStyleInterpolator:CardStyleInterpolators.forHorizontalIOS}
+              }/>
+              
+              <Stack.Screen name="Drawer" component={Drawer_Navigation} options={
+                  {cardStyleInterpolator:CardStyleInterpolators.forHorizontalIOS}
+              } />
+  
+          </Stack.Navigator>
+          
+      </NavigationContainer>
+    )
+}
+
+export default StackNavigation
